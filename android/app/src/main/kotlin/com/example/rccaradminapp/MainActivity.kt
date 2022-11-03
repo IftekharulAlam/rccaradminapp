fa846e0d8fc4dae01590daea21b3cd992c9a7d46
Iftekharul Alam <iftekharulalam38@gmail.com>
2022-11-03 13:17:55 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/rccaradminapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rccaradminapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: sort_child_properties_last,, use_build_context_synchronously
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: const Text("RC Car Admin ")),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  Future writeData(String deviceID, String command) async {
+    http.Response response = await http.post(
+        Uri.parse("http://192.168.0.100:8000/writeStatus"),
+        body: {"command": command, "deviceID": deviceID});
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception("Error loading data");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: ListView(
+        children: <Widget>[
+          Container(
+            height: 50,
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: ElevatedButton(
+              child: const Text('Login'),
+              onPressed: () {
+                //  login();
+                writeData("1", "F");
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
